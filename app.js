6c4d6adb64eb8fd80e91ecf4cc641428f68fbfb9
Kevin Kohlhase <kbkohlhase@gmail.com>
2016-02-11 21:19:46 +0000

    ajax lab

--- app.js
+++ app.js
@@ -10,8 +10,10 @@ var handlebars = require('express3-handlebars')
 
 var index = require('./routes/index');
 var project = require('./routes/project');
+var palette = require('./routes/palette');
 // Example route
 // var user = require('./routes/user');
+
 
 var app = express();
 
@@ -38,8 +40,10 @@ if ('development' == app.get('env')) {
 // Add routes here
 app.get('/', index.view);
 app.get('/project/:id', project.projectInfo);
+app.get('/palette', palette.randomPalette);
 // Example route
 // app.get('/users', user.list);
+
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -27,6 +27,9 @@ function addProjectDetails(e) {
 	var idNumber = projectID.substr('project'.length);
 
 	console.log("User clicked on project " + idNumber);
+
+	$.get("/project/"+ idNumber, expandProject);
+	console.log("calling " + "/project/"+ idNumber)
 }
 
 /*
@@ -35,4 +38,33 @@ function addProjectDetails(e) {
  */
 function randomizeColors(e) {
 	console.log("User clicked on color button");
+	e.preventDefault();
+
+	$.get("/palette", getColors);
+	console.log("calling " + "/palette/")
+}
+
+function getColors(result)
+{
+	console.log(result['colors']);
+
+	var colorr = result['colors'];
+	console.log(colorr.hex);
+	var colors = colorr.hex;
+	$('body').css('background-color', colors[0]);
+	$('.thumbnail').css('background-color', colors[1]);
+	$('h1, h2, h3, h4, h5, h5').css('color', colors[2]);
+	$('p').css('color', colors[3]);
+	$('.project img').css('opacity', .75);
+
+}
+
+function expandProject(result)
+{
+	console.log(result);
+	console.log($("#project" +result['id'] + " .details").length);
+	var projectDescription = '<img src="' + result['image'] + '" class = "detailsImage">' + 
+	'<h3>' + result['title'] + '</h3>' + '<p>' + result['date'] + '</p>' +
+	 '<p>' + result['summary'] + '</p>'
+	$("#project" + result['id']+" .details" ).html(projectDescription);
 }
